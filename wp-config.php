fd35f56799c8c94cc520ce1207e083b5c3f8f3e5
Lia <125735428+Aulialiap@users.noreply.github.com>
2024-12-22 19:14:28 +0700

    Create wp-config.php

---
+++ wp-config.php
@@ -0,0 +1,8 @@
+define('WP_REDIS_HOST', 'redis');
+define('WP_REDIS_PORT', 6379);
+define('WP_REDIS_PASSWORD', 'redis_password');  // Password sesuai dengan yang ada di docker-compose.yml
+define('WP_REDIS_TIMEOUT', 1);
+define('WP_REDIS_READ_TIMEOUT', 1);
+define('WP_REDIS_DATABASE', 0);
+
+/* That's all, stop editing! Happy publishing. */
